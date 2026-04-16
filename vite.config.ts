9a48e222c2f4ea3493dbdb1b4fc3e96837d262cd
danielo <hamyung1234@gmail.com>
2026-04-16 13:43:34 +0900

    chore: change base path from /-nabi-app-/ to /nabi7app/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/-nabi-app-/',
+  base: '/nabi7app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
